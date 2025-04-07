5d1877dc81b44f6c87395fa0dc4e507d62105cc7
Rostyslav Andrusevych <76594620+Eldemerea@users.noreply.github.com>
2025-04-07 17:28:53 +0300

    Initial Commit

---
+++ ToDoCombine/ToDoCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoCombineApp.swift
+//  ToDoCombine
+//
+//  Created by mac on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
