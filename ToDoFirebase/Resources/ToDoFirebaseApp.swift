44882eb816577996ba3de02a603a78520a615acf
Rostyslav Andrusevych <76594620+Eldemerea@users.noreply.github.com>
2025-04-24 22:17:05 +0300

    File structure changes

--- ToDoFirebase/Resources/ToDoFirebaseApp.swift
+++ ToDoFirebase/Resources/ToDoFirebaseApp.swift
@@ -4,11 +4,16 @@
 //
 //  Created by mac on 07.04.2025.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoFirebaseApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
