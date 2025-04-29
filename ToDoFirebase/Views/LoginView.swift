0423aaea489b9d480fbc21a257bb57fd5e95031f
Rostyslav Andrusevych <76594620+Eldemerea@users.noreply.github.com>
2025-04-29 23:42:20 +0300

    Minor changes

--- ToDoFirebase/Views/LoginView.swift
+++ ToDoFirebase/Views/LoginView.swift
@@ -8,15 +8,29 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    @State private var showPassword = false
         var body: some View {
             
             VStack {
-                Text("to:do")
-                    .foregroundColor(.black)
-                    .font(Font.custom("KyivTypeSerif-Light", size: 36))
-                    .frame(width: 200, height: 100)
-                    .padding(.top, 30)
+                    Text("to:do")
+                        .foregroundColor(.black)
+                        .font(Font.custom("KyivTypeSerif-Light", size: 36))
+//                        .frame(width: 200, height: 100)
+                        .padding(.top, 30)
                 Spacer()
+                Form {
+                    TextField("EMail address", text: $email)
+                    HStack {
+                        SecureField("Password", text: $password)
+                        Button(action: { self.showPassword.toggle()}) {
+                            Image(systemName: "eye")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
             }
         }
 }
